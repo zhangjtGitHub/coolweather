26a6a7da00d8425a20f8cc5cc6950ca8c10d0762
jiantao.zhang <jiantao.zhang@jrdcom.com>
2017-04-29 15:53:59 +0800

    add database and config of table

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,46 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zjt on 17-4-29.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
